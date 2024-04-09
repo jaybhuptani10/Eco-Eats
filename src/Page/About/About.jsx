1e5fff008c26616f5563d215d1e6a1e03303e469
jaybhuptani10 <115783493+jaybhuptani10@users.noreply.github.com>
2024-04-09 14:35:57 +0530

    Update About.jsx

--- src/Page/About/About.jsx
+++ src/Page/About/About.jsx
@@ -13,23 +13,15 @@ const About = () => {
           Jay Bhuptani{" "}
           <div className="user">Front-End Developer and Presentation</div>
         </h1>
-        {/* <h1>
+         <h1>
           Varun Jethani <div className="user">Chat-Bot Developer</div>
-        </h1> */}
+        </h1> 
         <h1>
           Vidhan Prajapti{" "}
           <div className="user">Back-End Developer and Testor</div>
         </h1>
-        <h1 className="jag">
-          Jagdish Khandelwal{" "}
-          <div className="user jag">Presentation and Report</div>
-        </h1>
-        <h1>
-          Kirtan Chauhan<div className="user">Report and Tester</div>
-        </h1>
-        <h1>
-          Krishna Soni <div className="user">Report</div>
-        </h1>
+        
+        
       </div>
     </div>
   );
